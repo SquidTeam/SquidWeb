36953a6aca17b47e8bbf2740e1718b673c629d54
Bastien Dos Santos <bastiendos@gmail.com>
2016-04-14 21:10:34 +0200

    Ajout classes

---
+++ SquidWeb/src/Squidteam/ihm/Serveur.java
@@ -0,0 +1,30 @@
+package Squidteam.ihm;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.DefaultServlet;
+import org.eclipse.jetty.servlet.ServletHolder;
+import org.eclipse.jetty.webapp.WebAppContext;
+
+public class Serveur {
+
+
+  public Serveur() {
+    // TODO Auto-generated constructor stub
+
+  }
+
+
+  public static void lancerServeur() throws Exception {
+    Server server = new Server(8080);
+    WebAppContext context = new WebAppContext();
+    context.setContextPath("/");
+    context.addServlet(new ServletHolder(new MainServlet()), "/mobilitas/*");
+    context.addServlet(new ServletHolder(new DefaultServlet()), "/");
+    context.setResourceBase("web");
+    context.setWelcomeFiles(new String[] {"web/index.html"});
+    context.setInitParameter("cacheControl", "no-store,no-cache,must-revalidate");
+    server.setHandler(context);
+    server.start();
+  }
+}
+
--- SquidWeb/src/Squidteam/main/Main.java
+++ SquidWeb/src/Squidteam/main/Main.java
@@ -1,71 +1,69 @@
 package Squidteam.main;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
-
+import Squidteam.ihm.Serveur;
 
 public class Main {
 
   public static void main(String[] args) {
-    /* Chargement du driver JDBC pour MySQL */
+
+
+    /*
+     * try {
+     * 
+     * Class.forName("com.mysql.jdbc.Driver");
+     * 
+     * } catch (ClassNotFoundException e) {
+     * 
+     * System.out.println("Driver manquant !");
+     * 
+     * }
+     * 
+     * String url = "jdbc:mysql://vps254243.ovh.net:3306/SquidDB";
+     * 
+     * String utilisateur = "Squid";
+     * 
+     * String motDePasse = "squidpw";
+     * 
+     * Connection connexion = null; String a = "a"; String b = "b";
+     * 
+     * if (a == b) { System.out.println(a); System.out.println(b); }
+     * 
+     * try {
+     * 
+     * connexion = DriverManager.getConnection(url, utilisateur, motDePasse);
+     * 
+     * System.out.println("Connexion ok !"); Statement statement = connexion.createStatement(); int
+     * statut = statement.executeUpdate("INSERT INTO users(firstname) VALUES ('Benjamin')");
+     * System.out.println(statut); } catch (SQLException e) {
+     * 
+     * System.out.println("Serveur injoignable");
+     * 
+     * } finally {
+     * 
+     * if (connexion != null)
+     * 
+     * try {
+     * 
+     * Fermeture de la connexion
+     * 
+     * connexion.close();
+     * 
+     * } catch (SQLException ignore) {
+     * 
+     * 
+     * Si une erreur survient lors de la fermeture, il suffit de l'ignorer.
+     * 
+     * 
+     * }
+     * 
+     * }
+     */
 
     try {
-
-      Class.forName("com.mysql.jdbc.Driver");
-
-    } catch (ClassNotFoundException e) {
-
-      System.out.println("Driver manquant !");
-
-    }
-
-    String url = "jdbc:mysql://vps254243.ovh.net:3306/SquidDB";
-
-    String utilisateur = "Squid";
-
-    String motDePasse = "squidpw";
-
-    Connection connexion = null;
-    String a = "a";
-    String b = "b";
-
-    if (a == b) {
-      System.out.println(a);
-      System.out.println(b);
-    }
-
-    try {
-
-      connexion = DriverManager.getConnection(url, utilisateur, motDePasse);
-
-      System.out.println("Connexion ok !");
-      Statement statement = connexion.createStatement();
-      int statut = statement.executeUpdate("INSERT INTO users(firstname) VALUES ('Benjamin')");
-      System.out.println(statut);
-    } catch (SQLException e) {
-
-      System.out.println("Serveur injoignable");
-
-    } finally {
-
-      if (connexion != null)
-
-        try {
-
-          /* Fermeture de la connexion */
-
-          connexion.close();
-
-        } catch (SQLException ignore) {
-
-          /*
-           * Si une erreur survient lors de la fermeture, il suffit de l'ignorer.
-           */
-
-        }
-
+      Serveur.lancerServeur();
+    } catch (Exception e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
     }
   }
 }
